0c23ca09c9f88650004e9682c34da9ab6fc66d97
Ronny Steinbrenner <ronny.steinbrenner@comsysto.com>
2017-08-02 10:55:27 +0200

    Add two spring boot services

---
+++ service-1/src/main/java/com/comsysto/lab/service1/Service1Application.java
@@ -0,0 +1,12 @@
+package com.comsysto.lab.service1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Service1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Service1Application.class, args);
+	}
+}
