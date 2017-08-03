6ff291958656e09fa68857e7bf1158d31b07c6b8
Matthias Drews <matthias.drews@comsysto.com>
2017-08-03 11:21:47 +0200

    remove startup and shutdown

--- service-1/shutdown.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ps aux | grep service-1-0.0.1-SNAPSHOT.jar | awk '{print $2}' | xargs kill -9
--- service-1/startup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-nohup java -jar jenkinslab/service-1/service-1-0.0.1-SNAPSHOT.jar --server.port=9090 &
